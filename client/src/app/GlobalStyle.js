92163b9ba5cf6782e92ef24de873afc675d5d599
DevHamburg <guberinic040@gmail.com>
2019-04-22 15:11:59 +0200

    Windows package.json fix

--- client/src/app/GlobalStyle.js
+++ client/src/app/GlobalStyle.js
@@ -6,17 +6,22 @@ export default createGlobalStyle`
   }
 
   body {
+    /* max-width: 1366px;
+    max-height: 1024px; */
     font-family: Roboto, sans-serif;
     -webkit-font-smoothing: antialiased;
-    margin: 0;
+    border: 1px solid lightgray;
   }
 
   html, body {
     font-family: Roboto, sans-serif;
     -webkit-font-smoothing: antialiased;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     position: fixed;
     width: 100%;
-    height: 100%;
+    height: 100vh;
     overflow: hidden;
   }
 
